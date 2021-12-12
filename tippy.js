0c29aa5709ea4d2e58c16b7a935089ca354a08a0
Shanakay Hall <41412462+skayhall@users.noreply.github.com>
2021-12-11 23:39:31 -0500

    added comments to js

--- tippy.js
+++ tippy.js
@@ -1,35 +1,41 @@
-let tip10 = 0.10 ;
+/*let tip10 = 0.10 ;
 let tip15 = 0.15;
-let tip20 = 0.20;
+let tip20 = 0.20;*/
 
 
 
-
+/*function to calculate tip amount */
 function calcTip () {
 
+    /*grab bill amount */
     let cost = document.querySelector('#tip-input').value;
+    /*convert cost to number from string */
     let finalCost = Number(cost);
+    /*display cost */
     document.querySelector("#cost-label").innerHTML=`Cost: $ ${finalCost}`;
-    console.log("Number variable is" + typeof finalCost);
+    /*console.log("Number variable is" + typeof finalCost);*/
 
+    /*grab cost and multiply by tip amount, then display display tip amount*/
     let tipAmount = document.querySelector('#tip-input').value * tip10;
     document.querySelector("#tip-label").innerHTML=`Tip Amount: $ ${tipAmount}`;
-    console.log(typeof tipAmount);
+    /*console.log(typeof tipAmount);*/
 
-
+    /*add cost and tip amount to get total cost */
     let ttlCost = finalCost + tipAmount;
     let ttlCostconv = Number(ttlCost);
     let finalttlCost = ttlCost + tipAmount;
 
+    /*display final cost */
     document.querySelector("#total-label").innerHTML= "Final Cost: " + ttlCost;
     
-    console.log(typeof finalttlCost);
+    /*console.log(typeof finalttlCost);*/
 
     /*grab selected tip amount*/
     let sel = document.getElementById('tip-amount');
     /* print tip amount to console */
     console.log( sel.value );
 
+    /*check value chosen and assign ti[p ammouny */
     if (sel.value === "10ten") {
 
         let tipValue = 0.10;
@@ -49,8 +55,7 @@ function calcTip () {
 
     }
    
-
-
+    /*return tip amount */
     return tipAmount;
 
 }
