8317308597fc7aa02d4435171480ded1542d7cae
Shanakay Hall <41412462+skayhall@users.noreply.github.com>
2021-12-11 23:30:27 -0500

    edit css and added tip percentage

--- tippy.js
+++ tippy.js
@@ -2,6 +2,9 @@ let tip10 = 0.10 ;
 let tip15 = 0.15;
 let tip20 = 0.20;
 
+
+
+
 function calcTip () {
 
     let cost = document.querySelector('#tip-input').value;
@@ -21,6 +24,31 @@ function calcTip () {
     document.querySelector("#total-label").innerHTML= "Final Cost: " + ttlCost;
     
     console.log(typeof finalttlCost);
+
+    /*grab selected tip amount*/
+    let sel = document.getElementById('tip-amount');
+    /* print tip amount to console */
+    console.log( sel.value );
+
+    if (sel.value === "10ten") {
+
+        let tipValue = 0.10;
+        console.log(tipValue);
+
+    } else if (sel.value === "15fifteen") {
+
+        let tipValue = 0.15;
+        console.log(tipValue);
+
+    }
+
+        else if (sel.value === "20twenty") {
+
+            let tipValue = 0.20;
+            console.log(tipValue);
+
+    }
+   
 
 
     return tipAmount;
