22cab1f2dc00bbe129750ae4aaeed21c2225fac9
Shanakay Hall <41412462+skayhall@users.noreply.github.com>
2021-12-12 00:03:56 -0500

    remove unused js, change let to var if stmt

--- tippy.js
+++ tippy.js
@@ -1,9 +1,3 @@
-/*let tip10 = 0.10 ;
-let tip15 = 0.15;
-let tip20 = 0.20;*/
-
-
-
 /*function to calculate tip amount */
 function calcTip () {
 
@@ -15,72 +9,51 @@ function calcTip () {
     document.querySelector("#cost-label").innerHTML=`Cost: $ ${finalCost}`;
     /*console.log("Number variable is" + typeof finalCost);*/
 
-    /*grab cost and multiply by tip amount, then display display tip amount*/
-    let tipAmount = document.querySelector('#tip-input').value * tip10;
-    document.querySelector("#tip-label").innerHTML=`Tip Amount: $ ${tipAmount}`;
-    /*console.log(typeof tipAmount);*/
-
-    /*add cost and tip amount to get total cost */
-    let ttlCost = finalCost + tipAmount;
-    let ttlCostconv = Number(ttlCost);
-    let finalttlCost = ttlCost + tipAmount;
-
-    /*display final cost */
-    document.querySelector("#total-label").innerHTML= "Final Cost: " + ttlCost;
-    
-    /*console.log(typeof finalttlCost);*/
 
     /*grab selected tip amount*/
     let sel = document.getElementById('tip-amount');
     /* print tip amount to console */
     console.log( sel.value );
 
-    /*check value chosen and assign ti[p ammouny */
+    /*check value chosen and assign tip ammouny */
     if (sel.value === "10ten") {
 
-        let tipValue = 0.10;
+        var tipValue = 0.10;
         console.log(tipValue);
 
     } else if (sel.value === "15fifteen") {
 
-        let tipValue = 0.15;
+        var tipValue = 0.15;
         console.log(tipValue);
 
     }
 
         else if (sel.value === "20twenty") {
 
-            let tipValue = 0.20;
+            var tipValue = 0.20;
             console.log(tipValue);
 
     }
-   
-    /*return tip amount */
-    return tipAmount;
+
+
+    /*grab cost and multiply by tip amount, then display display tip amount*/
+    let tipAmount = document.querySelector('#tip-input').value * `${tipValue}`;
+    document.querySelector("#tip-label").innerHTML=`Tip Amount: $ ${tipAmount}`;
+    console.log(typeof tipAmount);
+
+    /*add cost and tip amount to get total cost */
+    let ttlCost = finalCost + tipAmount;
+    /*let ttlCostconv = Number(ttlCost);
+    let finalttlCost = ttlCost + tipAmount;*/
+
+    /*display final cost */
+    document.querySelector("#total-label").innerHTML= "Final Cost: " + ttlCost;
+    
+    /*console.log(typeof finalttlCost);*/
+
 
 }
 
-/*
-function getInput () {
 
-    let personName = document.querySelector('#name-input').value;
-    console.log(`${personName}`)
-
-    if (personName === "Shanakay" || personName === "Kay") {
-
-        console.log(`Correct + ${personName}`)
-        document.querySelector("#name-label").innerHTML="Correct";
-
-
-    } else {
-
-        console.log("Wrong")
-        document.querySelector("#name-label").innerHTML="Wrong";
-
-    }
-
-}
-
-*/
 
 
